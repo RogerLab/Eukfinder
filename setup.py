bc92af6c450983b0323151aa36431e5d29e65b29
Jason Shao <Jason.Shao@dal.ca>
2024-09-03 18:54:56 -0300

    feat: updated setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="eukfindertest2",
-    version="1.0.1",
+    name="eukfinder",
+    version="1.2.3",
     author="dzhao",
     author_email="dandan.tanny.zhao@email.com",
-    description="A small demo package",
+    description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dzhao2019/eukfindertest",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=([
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+        "Operating System :: OS Independent"
+    ]),
+    py_modules=["Eukfinder"],
+    entry_points={
+        'console_scripts': [
+            'eukfinder = Eukfinder:main',
+        ],
+    }
 )
